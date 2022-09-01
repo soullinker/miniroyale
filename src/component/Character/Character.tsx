3084ae9db9a0abe1d15542bbe3066b02b0a17e0b
Vitaly Melkishev <soullinker0@gmail.com>
2022-09-01 20:58:41 +0300

    small style fix

--- src/component/Character/Character.tsx
+++ src/component/Character/Character.tsx
@@ -44,21 +44,23 @@ class Character extends RxComponent<CharacterProps, CharacterState> {
 
         return character ? <div className="character">
             <h1>{character.name}</h1>
-            <div>Weight: {character.mass}</div>
-            <div>name: {character.name}</div>
-            <div>height: {character.height}</div>
-            <div>mass: {character.mass}</div>
-            <div>hairColor: {character.hairColor}</div>
-            <div>skinColor: {character.skinColor}</div>
-            <div>eyeColor: {character.eyeColor}</div>
-            <div>birthYear: {character.birthYear}</div>
-            <div>gender: {character.gender}</div>
-            <div>homeWorld: {character.homeWorld.name}</div>
-            <div>film count: {character.filmList.length}</div>
-            <div>vehicle count: {character.vehicleList.length}</div>
-            <div>starship count: {character.starshipList.length}</div>
-            <div>created: {character.created.format('Y-m-d H:i:s')}</div>
-            <div>edited: {character.edited.format('Y-m-d H:i:s')}</div>
+            <div className="character__props">
+                <div>Weight: {character.mass}</div>
+                <div>name: {character.name}</div>
+                <div>height: {character.height}</div>
+                <div>mass: {character.mass}</div>
+                <div>hairColor: {character.hairColor}</div>
+                <div>skinColor: {character.skinColor}</div>
+                <div>eyeColor: {character.eyeColor}</div>
+                <div>birthYear: {character.birthYear}</div>
+                <div>gender: {character.gender}</div>
+                <div>homeWorld: {character.homeWorld.name}</div>
+                <div>film count: {character.filmList.length}</div>
+                <div>vehicle count: {character.vehicleList.length}</div>
+                <div>starship count: {character.starshipList.length}</div>
+                <div>created: {character.created.format('Y-m-d H:i:s')}</div>
+                <div>edited: {character.edited.format('Y-m-d H:i:s')}</div>
+            </div>
         </div> : <div>loading...</div>;
     }
 }
